e7bb7cc0b10e78ced2abb4957f75aba6b69d9764
Phalluis <nicholaslonganilla@gmail.com>
2024-06-17 22:18:25 +0800

    Update Branch_update.php

--- Branch_update.php
+++ Branch_update.php
@@ -1,49 +1,57 @@
 <?php
+include('connection.php');
 
-	include('connection.php');
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$id = $_GET['updateid'];
-		$strBranchname = $_POST['strBranchname'];
-		$strBranchAddress = $_POST['strBranchAddress'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_GET['updateid'])) {
+        $id = $_GET['updateid'];
+        $strBranchname = mysqli_real_escape_string($conn, $_POST['strBranchname']);
+        $strBranchAddress = mysqli_real_escape_string($conn, $_POST['strBranchAddress']);
 
-		$sql = mysqli_query($conn, "UPDATE branchtable set strBranchname = '$strBranchname', strBranchAddress = '$strBranchAddress' WHERE intBranchID = '$id' " );
-		if($sql) {
-			echo "<script>alert('Pasok na pasok for the second time')</script>";
-			echo "<script type = 'text/javascript'> document.location = 'Branch_view.php'</script>";
-		}else{
-			echo "<script>alert('Hindi Pasok')</script>";
-			echo "<script type = 'text/javascript'> document.location = 'Branch_view.php'</script>";
-		}
-	}
-	else{
-		echo "<script>alert('Nigga walang branch Id')</script>";
-		echo "<script type = 'text/javascript'> document.location = 'Branch_view.php'</script>";
-	}
+        $sql = "UPDATE branchtable SET strBranchname = '$strBranchname', strBranchAddress = '$strBranchAddress' WHERE intBranchID = '$id'";
+        if (mysqli_query($conn, $sql)) {
+            echo "<script>alert('Pasok na pasok for the second time');</script>";
+            echo "<script>document.location = 'Branch_view.php';</script>";
+        } else {
+            echo "<script>alert('Hindi Pasok');</script>";
+            echo "<script>document.location = 'Branch_view.php';</script>";
+        }
+    } else {
+        echo "<script>alert('Walang branch ID');</script>";
+        echo "<script>document.location = 'Branch_view.php';</script>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Branch Update</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Branch Update</title>
 </head>
 <body>
-	<form method="POST">
-		<?php
-			$id = $_GET['updateid'];
-				$query = mysqli_query($conn, "SELECT * FROM branchtable WHERE intBranchID = '$id'");
-				while ($row = mysqli_fetch_array($query)) {
-		?>
-		<label for="strBranchname">Branch Name</label>
-		<input type="text" value = "<?php echo $row['strBranchname']?>" name="strBranchname" required><br>
+    <form method="POST">
+        <?php
+        if (isset($_GET['updateid'])) {
+            $id = $_GET['updateid'];
+            $query = mysqli_query($conn, "SELECT * FROM branchtable WHERE intBranchID = '$id'");
+            if ($row = mysqli_fetch_array($query)) {
+        ?>
+        <label for="strBranchname">Branch Name</label>
+        <input type="text" value="<?php echo htmlspecialchars($row['strBranchname']); ?>" name="strBranchname" required><br>
 
-		<label for="strBranchAddress">Branch Address</label>
-		<input type="text" value = "<?php echo $row['strBranchAddress']?>" name="strBranchAddress" required><br>
+        <label for="strBranchAddress">Branch Address</label>
+        <input type="text" value="<?php echo htmlspecialchars($row['strBranchAddress']); ?>" name="strBranchAddress" required><br>
 
-		<?php } // while bracket end ?>
-
-		<input type="submit" name="submit">
-	</form>
+        <input type="submit" value="Update">
+        <?php
+            } else {
+                echo "<p>No branch found with ID: $id</p>";
+            }
+        } else {
+            echo "<p>No branch ID provided.</p>";
+        }
+        ?>
+    </form>
 </body>
 </html>
