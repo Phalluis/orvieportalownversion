24a1653c9bfc8e89694b0054901bb86896729161
Phalluis <nicholaslonganilla@gmail.com>
2024-06-18 01:50:44 +0800

    commit sewerslide 2

--- Membership_Extend.php
+++ Membership_Extend.php
@@ -1,10 +1,15 @@
 <?php
 	include('connection.php');
 	if (isset($_GET['memberid'])) {
+        //stores the global variable $_GET['memberid'] into $id
 		$id = $_GET['memberid'];
+        //creates a quesry
 		$query = mysqli_query($conn, "SELECT intMemEnd FROM membershiptable WHERE intMemID=$id");
+        //if the query returns a result of more than 0 then it found a match
         if (mysqli_num_rows($query) > 0) {
+            //stores the row in $row
             $row = mysqli_fetch_assoc($query);
+            //stores the column intMemEnd in $currentEndDate
             $currentEndDate = $row['intMemEnd'];
         
             //stores the new enddate in $newEndDate
@@ -14,13 +19,16 @@
             $updateQuery = mysqli_query($conn, "UPDATE membershiptable SET intMemEnd='$newEndDate' WHERE intMemID=$id");
             
             if ($updateQuery) {
+                //if successful ang $updateQuery
                 echo "<script>alert('Extended')</script>";
 			    echo "<script type= 'text/javascript'> document.location = 'Membership_view.php'</script>";
             } else {
+                //if hinde
                 echo "<script>alert('Membership Extension Failed');</script>";
                 echo "<script type='text/javascript'> document.location = 'Membership_view.php'</script>";
             }
         } else {
+            //if wala syang match s $query
             echo "<script>alert('Nigga walang Member');</script>";
             echo "<script type='text/javascript'> document.location = 'Membership_view.php'</script>";
         }
